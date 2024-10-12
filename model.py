957e936cd5b75117cc18796c389ec9ee19c47567
zzhou <zzhou@vega.com>
2024-10-12 13:59:59 +0800

    Init Model

--- model.py
+++ model.py
@@ -1,13 +1,80 @@
 import torch
 from torch import nn
 
+
 class YOLOv1(nn.Module):
     def __init__(self):
         super(YOLOv1, self).__init__()
+        layers = [
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),  # Conv 1
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),  # Conv 2
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(192, 128, kernel_size=1),  # Conv 3
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(256, 256, kernel_size=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        for i in range(4):  # Conv 4
+            layers += [
+                nn.Conv2d(512, 256, kernel_size=1),
+                nn.Conv2d(256, 512, kernel_size=3, padding=1),
+                nn.LeakyReLU(negative_slope=0.1)
+            ]
+        layers += [
+            nn.Conv2d(512, 512, kernel_size=1),
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        for i in range(2):  # Conv 5
+            layers += [
+                nn.Conv2d(1024, 512, kernel_size=1),
+                nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+                nn.LeakyReLU(negative_slope=0.1)
+            ]
+        layers += [
+            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+        ]
+
+        for _ in range(2):  # Conv 6
+            layers += [
+                nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+                nn.LeakyReLU(negative_slope=0.1)
+            ]
+
+        layers += [
+            nn.Flatten(),
+            nn.Linear(7 * 7 * 1024, 4096),  # Linear 1
+            nn.Dropout(),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Linear(4096, 7 * 7 * 30),  # Linear 2
+        ]
+
+        self.model = nn.Sequential(*layers)
 
     def forward(self, x):
-        return x
+        batch_size = x.size()[0]
+        x = self.model(x)
+        return torch.reshape(x, [batch_size, 7, 7, 30])
 
 
 if __name__ == '__main__':
+    input = torch.rand(1, 3, 448, 448)
     model = YOLOv1()
+    output = model(input)
+    print(output.size())
